6cebed7150cc8ac0097564df24e9fe51fb37bf2f
UpSideDownGitHub <65664827+UpSideDownGitHub@users.noreply.github.com>
2024-02-29 19:55:58 +0000

    Init Push

---
+++ CRayCaster/Main.c
@@ -0,0 +1,44 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include "raycaster.h"
+
+int main(int argc, char* argv) {
+	// initilise values
+	double posX = initXPos, posY = initYPos;
+	double dirX = initXDir, dirY = initYDir;
+	double planeX = initPlaneX, planeY = initPlaneY;
+	double time = 0;
+	double oldTime = 0;
+
+	// setup renderer
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		fprintf(stderr, "SDL initialization failed (%s)\n", SDL_GetError());
+		return -1;
+	}
+	if (SDL_CreateWindowAndRenderer(screenWidth, screenHeight, 0, window, renderer) != 0) {
+		fprintf(stderr, "Window creation failed (%s)\n", SDL_GetError());
+		return -1;
+	}
+
+	// main loop
+	SDL_bool done;
+	done = SDL_FALSE;
+	while (!done) {
+		for(int x = 0; x < screenWidth; x++){
+			// ray pos & Dir
+			double cameraX = 2 * x / (double)screenWidth - 1;
+			double rayDirX = dirX + planeX * cameraX;
+			double rayDirY = dirY + planeY * cameraX;
+
+			int mapX = (int)posX;
+			int mapY = (int)posY;
+
+			double sideDistX;
+
+		}
+	}
+
+
+	return 0;
+}
